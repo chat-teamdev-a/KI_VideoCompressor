62f237f42faf9749ff89d1506ade10e3b3eda4bf
Kohei II <pprograming4@gmail.com>
2024-05-08 07:17:42 +0900

    add errorhandling

--- clientv.py
+++ clientv.py
@@ -29,9 +29,16 @@ def send_request(command_number, file_name, output_file, address, port):
             if not chunk:
                 break
             data += chunk
-        with open(output_file, "wb") as f:
-            f.write(data)
-        print(f"Video processed and downloaded successfully as {output_file}.")
+        if data.startswith(b'{"error_code":'):
+            error_data = json.loads(data)
+            print(f"Error code: {error_data['error_code']}")
+            print(f"An error occurred: {error_data['description']}")
+            print(f"solution: {error_data['solution']}")
+        else:
+            with open(output_file, "wb") as f:
+                f.write(data)
+            print(f"Video processed and downloaded successfully as {output_file}.")
+
 
 if __name__ == "__main__":
     command_number = input("Enter the command number (1: Compress, 2: Change resolution, 3: Change the video aspect ratio, 4: Convert video to audio, 5: Create GIFs): ")
